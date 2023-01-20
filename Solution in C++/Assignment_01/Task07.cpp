db32c9413ac18d2bf09498d8041284311939a3f3
Arafat Sarkar <98816004+Anon3605@users.noreply.github.com>
2023-01-20 18:16:21 +0600

    Update Task07.cpp

--- Solution in C++/Assignment_01/Task07.cpp
+++ Solution in C++/Assignment_01/Task07.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 int main(){
     float a,b,c,d,result;
+    int result2;
     cin>>a>>b>>c>>d;
     result=pow(a,c);
-    cout<<result+b*a-(d/3);
+    result2=result+b*a-(d/3);
+    cout<<result2;
 }
